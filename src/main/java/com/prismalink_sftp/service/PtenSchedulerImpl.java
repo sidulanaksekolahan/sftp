a5aa40a334b7147222665e3b87d05e474bc5e26b
sidulanaksekolahan <mirfan.duri@gmail.com>
2026-06-11 00:33:25 +0700

    disable scheduler

--- src/main/java/com/prismalink_sftp/service/PtenSchedulerImpl.java
+++ src/main/java/com/prismalink_sftp/service/PtenSchedulerImpl.java
@@ -1,22 +1,22 @@
-package com.prismalink_sftp.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-public class PtenSchedulerImpl implements PtenScheduler {
-
-    private final PtenService ptenService;
-
-    @Autowired
-    public PtenSchedulerImpl(PtenService ptenService) {
-        this.ptenService = ptenService;
-    }
-
-    @Scheduled(fixedDelay = 300000) // 5 menit
-    @Override
-    public void checkFolder() {
-        ptenService.processNewFolders();
-    }
-}
+//package com.prismalink_sftp.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class PtenSchedulerImpl implements PtenScheduler {
+//
+//    private final PtenService ptenService;
+//
+//    @Autowired
+//    public PtenSchedulerImpl(PtenService ptenService) {
+//        this.ptenService = ptenService;
+//    }
+//
+//    @Scheduled(fixedDelay = 300000) // 5 menit
+//    @Override
+//    public void checkFolder() {
+//        ptenService.processNewFolders();
+//    }
+//}
