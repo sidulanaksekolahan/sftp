b4b035cb7d785ce47587b0d5097822c4cd2cd538
sidulanaksekolahan <mirfan.duri@gmail.com>
2026-06-10 18:31:59 +0700

    Added Spring Data REST dependency

--- src/main/java/com/prismalink_sftp/entity/ProcessedFolder.java
+++ src/main/java/com/prismalink_sftp/entity/ProcessedFolder.java
@@ -6,7 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @Entity
